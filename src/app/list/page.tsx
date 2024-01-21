af1123b1cf64fe99102db8f1025d855d2309a3a7
kshyatisekhar-panda <kshyatisekhar.panda@gmail.com>
2024-01-21 01:03:06 +0100

    Add config with app routing and add new routing

---
+++ src/app/list/page.tsx
@@ -0,0 +1,5 @@
+export default function List() {
+    return (
+        <h3>List is here</h3>
+    )
+}
---
+++ src/app/registration/page.tsx
@@ -0,0 +1,5 @@
+export default function Registration() {
+    return (
+        <h3>Registration is here</h3>
+    )
+}
